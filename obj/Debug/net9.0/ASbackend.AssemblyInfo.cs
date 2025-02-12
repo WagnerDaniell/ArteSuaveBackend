f098632b2e0f4c39f8841be498563f3a749a7404
WagnerDaniell <wagnerdaniell06@gmail.com>
2025-02-12 14:12:58 -0300

    update controller

--- obj/Debug/net9.0/ASbackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASbackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASbackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb6db4058b69294ada84b6aa4618eb2b63fed40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe0aa49547ec443a0ba61bb4fa611ed4e018c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASbackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASbackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
