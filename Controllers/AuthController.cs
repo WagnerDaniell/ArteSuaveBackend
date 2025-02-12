00eeae300f4da184004230347550f519ce495f25
WagnerDaniell <wagnerdaniell06@gmail.com>
2025-02-12 14:18:30 -0300

    update controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -17,6 +17,29 @@ namespace ASbackend.Controllers
         }
 
         [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<User>> RegisterUsers(User user){
+
+            var ExistingUser = await _context.Users.FirstOrDefaultAsync(u=> u.Email == user.Email); 
+
+            if(ExistingUser != null){
+                return BadRequest(new{message = "Email já utilizado!"});
+            };
+
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            var token = TokenService.GenerateToken(user);
+
+            return Created("api/as/register",new
+            {
+                token = token
+            });
+        }
+
+        [HttpPost]
         [Route("login")]
         public async Task<ActionResult<dynamic>> Authenticated([FromBody] UserLogin model)
         {
